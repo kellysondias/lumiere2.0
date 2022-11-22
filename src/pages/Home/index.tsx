0aad90a2a040b6dc344135f37bbc4db1b75aedcf
Kellyson <kellysondias@outlook.com>
2022-11-22 10:53:18 -0300

    Finalizando a página 'details'

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from "react";
 import { imgUrl } from "../../services/variables";
 import { Link } from "react-router-dom";
 import { Footer } from "../../components/footer/footer";
-import { MoviesSection, MovieCard } from "./styles";
+import { MovieCard } from "./styles";
 import "../../css/font-awesome-min.css";
 import { PageMenu } from "../../components/page-menu/page-menu";
 import { Movies } from "../../components/movies/movies";
 import { useLocalObservable, observer } from "mobx-react-lite";
 import { Store } from "./store";
+import { Flex, List, SimpleGrid, Spinner } from "@chakra-ui/react";
 
 const Home: React.FC = () => {
 	const store = useLocalObservable(() => new Store());
@@ -19,7 +20,7 @@ const Home: React.FC = () => {
 	return (
 		<Movies>
 			{store.moviesShelf.loader.isLoading ? (
-				<span>Loading</span>
+				<Spinner />
 			) : (
 				<>
 					<PageMenu
@@ -28,10 +29,18 @@ const Home: React.FC = () => {
 						onPreviousPage={store.moviesShelf.previousPage}
 					/>
 
-					<MoviesSection>
-						<ul>
+					<Flex
+						// direction="column"
+						justify="center"
+					>
+						<List
+							w="100%"
+							bg="red"
+							display="grid"
+							gridTemplateRows="repeat(auto-fit, minmax(250px, 1fr))"
+						>
 							{store.moviesShelf.items.length === 0 ? (
-								<span>Loading</span>
+								<Spinner />
 							) : (
 								store.moviesShelf.items.map((movie, index) => {
 									const rating = movie.vote_average.toFixed(1);
@@ -59,8 +68,8 @@ const Home: React.FC = () => {
 									);
 								})
 							)}
-						</ul>
-					</MoviesSection>
+						</List>
+					</Flex>
 
 					<PageMenu
 						page={store.moviesShelf.page}
